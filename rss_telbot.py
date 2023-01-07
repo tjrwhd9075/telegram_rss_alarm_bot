f7e6e84b1505c7274548db0ee5f36e252cfe3f01
tjrwhd9075 <33514656+tjrwhd9075@users.noreply.github.com>
2023-01-08 02:13:53 +0900

    Add files via upload

--- rss_telbot.py
+++ rss_telbot.py
@@ -460,7 +460,7 @@ async def get_avdbs_crawling(chat_id):
             "🕓 : "+content[4] + " | " + content[5] + "\n"+\
             "🖋 : " + writer + " | LV : " + lvl10 + lvl1 + "\n\n"+\
             "👀 : " + view + " | 💬 : " + recom + " | 👍 : " + good + "\n"+\
-            "제목 : ["+ title +"]("+content[1]+")" + "\n\n"+\
+            "제목 : ["+ title +"]("+content[0]+")" + "\n\n"+\
             contentTxt
 
         telbot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
