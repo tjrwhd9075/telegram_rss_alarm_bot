71314c9d5541e281f91d2ef6bba86f316148edd6
tjrwhd9075 <33514656+tjrwhd9075@users.noreply.github.com>
2023-01-08 22:34:19 +0900

    23.1.8.22

--- rss_telbot.py
+++ rss_telbot.py
@@ -465,7 +465,7 @@ async def get_avdbs_crawling(chat_id):
 
     #content : [num,thumb,boardType,adult,date,beforeTime,writer,lvl,view,recom,good,title,contentTxt]
     #           0   1     2         3     4    5          6      7   8    9     10   11    12
-    for content in newContents:
+    for content in newContents.reverse():
         thumb, adult, view, recom, good = "-","-","-","-","-"
         if content[1] is not None : thumb = content[1]
         if content[3] is not None : adult = "🔞"
